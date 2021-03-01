54c819d3b75995f78f0d7d0749e909cfcb314fc0
Your Name <you@example.com>
2021-03-01 00:09:38 -0600

    6 - Updating test files

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplWithDBTest.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplWithDBTest.java
@@ -1,35 +1,83 @@
 package com.lambdaschool.usermodel.services;
 
+import com.lambdaschool.usermodel.UserModelApplicationTesting;
+import com.lambdaschool.usermodel.exceptions.ResourceFoundException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
-public class UserServiceImplWithDBTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserModelApplicationTesting.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UserServiceImplWithDBTest
+{
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    HelperFunctions helperFunctions;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
     }
 
     @Test
-    public void findUserById() {
+    public void B_findUserById()
+    {
+        assertEquals("admin",
+                userService.findUserById(4)
+                    .getUsername());
+    }
+
+    @Test(expected = ResourceFoundException.class)
+    public void BA_findUserByIdNotFound()
+    {
+        assertEquals("admin",
+                userService.findUserById(10)
+                        .getUsername());
     }
 
     @Test
-    public void findByNameContaining() {
+    public void C_findAll()
+    {
+        assertEquals(5,
+                userService.findAll()
+                    .size());
     }
 
     @Test
-    public void findAll() {
+    public void D_delete()
+    {
+        userService.delete(13);
+        assertEquals(4,
+                userService.findAll()
+                    .size());
     }
 
     @Test
-    public void delete() {
+    public void DA_notFoundDelete()
+    {
+        userService.delete(100);
+        assertEquals(4,
+                userService.findAll()
+                        .size());
     }
 
     @Test
